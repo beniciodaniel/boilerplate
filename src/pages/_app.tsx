bbfea152cf8fab4e5f3bf5d3342f45edab4af68e
Benício Barreto <benicio.daniel@gmail.com>
2021-11-17 21:56:23 -0300

    Added GlobalStyles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="favicon.ico" />
+        <meta
+          name="description"
+          content="a simple project starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
