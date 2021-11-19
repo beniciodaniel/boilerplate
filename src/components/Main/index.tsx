0a97c93f78311e0dd90019c340df410bfcd589c1
Benício Barreto <benicio.daniel@gmail.com>
2021-11-19 18:04:12 -0300

    used styled component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled-Components
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
